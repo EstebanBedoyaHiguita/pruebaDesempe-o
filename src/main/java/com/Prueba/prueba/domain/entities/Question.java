fcd3b2022dda9a3c35a68ab8fddbf3497a645b52
EstebanBedoyaHiguita <estebanbedoyahiguita@gmail.com>
2024-06-04 09:35:25 -0500

    Relations Ok

--- src/main/java/com/Prueba/prueba/domain/entities/Question.java
+++ src/main/java/com/Prueba/prueba/domain/entities/Question.java
@@ -1,18 +1,27 @@
 package com.Prueba.prueba.domain.entities;
 
+import java.util.List;
+
 import com.Prueba.prueba.Utils.enums.Type;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Entity(name="question")
 @Data
@@ -27,6 +36,21 @@ public class Question {
     @Enumerated(EnumType.STRING)
     private Type type;
     @Column(nullable = false)
-    private String survey_id;
+    
     private Boolean active;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "survey_id",
+    referencedColumnName = "id")
+    private Survey survey;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "question",
+    fetch = FetchType.EAGER,
+    cascade = CascadeType.ALL,
+    orphanRemoval = false)
+    private List<OptionQuestion> optionQuestions;
+    
 }
--- src/main/java/com/Prueba/prueba/domain/entities/Survey.java
+++ src/main/java/com/Prueba/prueba/domain/entities/Survey.java
@@ -1,16 +1,24 @@
 package com.Prueba.prueba.domain.entities;
 
 import java.time.LocalDate;
+import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 
 @Entity(name = "survey")
 @Data
@@ -27,6 +35,20 @@ public class Survey {
     @Column(nullable = false)
     private LocalDate creation_date;
     @Column(nullable = false)
-    private String creator_id;
+    
     private Boolean active;
+
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "creator_id",
+    referencedColumnName = "id")
+    private User creator_idUser;
+
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    @OneToMany(mappedBy = "survey",
+    fetch = FetchType.EAGER,
+    cascade = CascadeType.ALL,
+    orphanRemoval = false)
+    private List<Question> questions;
 }
